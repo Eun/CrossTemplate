068943ad440826d929a903786836b90bfb8940b0
Eun <eun@su.am>
2013-10-22 12:57:24 +0200

    + Mac Bundle

--- macstart.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-# this is a start script for mac
-MONO_FRAMEWORK="/Library/Frameworks/Mono.framework/Versions/Current/"
-export DYLD_FALLBACK_LIBRARY_PATH="$MONO_FRAMEWORK/lib:/usr/local/lib:/usr/lib"
-EXE_DIR=`dirname $0`
-mono $MONO_OPTIONS $EXE_DIR/CrossTemplate.exe "$@"
